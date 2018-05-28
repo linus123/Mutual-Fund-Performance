0ba10a15d743b845a8c7015f4490676f29bddf0e
linus123 <linus_pagan@hotmail.com>
2018-05-28 06:25:04 -0500

    Added test data console

---
+++ src/TestDataPopulator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using MutualFundPerformance.Database.MutualFund;
+using MutualFundPerformance.SharedKernel.Infrastructure.MutualFundData;
+
+namespace MutualFundPerformance.TestDataPopulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testDataPopulatorSettings = new TestDataPopulatorSettings();
+
+            var mutualFundDataTableGateway = new MutualFundDataTableGateway(
+                testDataPopulatorSettings);
+
+            mutualFundDataTableGateway.DeleteAll();
+
+            var mutualFundDtos = new List<MutualFundDto>();
+
+            for (int fundCount = 0; fundCount < 20; fundCount++)
+            {
+                mutualFundDtos.Add(new MutualFundDto()
+                {
+                    MutualFundId = Guid.NewGuid(),
+                    Name = $"Mutual Fund {fundCount}"
+                });
+            }
+
+            Console.WriteLine("Inserting mutual funds.");
+
+            mutualFundDataTableGateway.Insert(
+                mutualFundDtos.ToArray());
+        }
+    }
+}
---
+++ src/TestDataPopulator/TestDataPopulatorSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using MutualFundPerformance.SharedKernel;
+
+namespace MutualFundPerformance.TestDataPopulator
+{
+    public class TestDataPopulatorSettings : IMutualFundPerformanceDatabaseSettings
+    {
+        public string MutualFundPerformanceDatabaseConnectionString
+        {
+            get
+            {
+                var configurationBuilder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+
+                var configurationRoot = configurationBuilder.Build();
+
+                return configurationRoot.GetConnectionString("MutualFundPerformanceDatabase");
+            }
+        }
+
+    }
+}
