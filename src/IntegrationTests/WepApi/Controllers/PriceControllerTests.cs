c31b9b372e7f26ad5f58ddcbbd6fb2c2312d99cd
linus123 <linus_pagan@hotmail.com>
2018-05-28 08:54:37 -0500

    Next test 4

--- src/IntegrationTests/WepApi/Controllers/PriceControllerTests.cs
+++ src/IntegrationTests/WepApi/Controllers/PriceControllerTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using FluentAssertions;
+using MutualFundPerformance.Database.HistoricalPriceData;
 using MutualFundPerformance.Database.MutualFund;
+using MutualFundPerformance.SharedKernel.Infrastructure.HistoricalPriceData;
 using MutualFundPerformance.SharedKernel.Infrastructure.MutualFundData;
 using MutualFundPerformance.WebApi.Controllers;
 using Xunit;
@@ -54,11 +56,12 @@ namespace MutualFundPerformance.IntegrationTests.WepApi.Controllers
             var symbol1 = "SYMB1";
 
             mutualFundDataTableGateway.Insert(new []{ new MutualFundDto()
-            {
-                MutualFundId = Guid.NewGuid(),
-                Name = "Some Fund",
-                Symbol = symbol1
-            }  });
+                {
+                    MutualFundId = Guid.NewGuid(),
+                    Name = "Some Fund",
+                    Symbol = symbol1
+                }
+            });
 
             var controller = new PriceController();
 
@@ -66,6 +69,49 @@ namespace MutualFundPerformance.IntegrationTests.WepApi.Controllers
 
             result.Should().HaveCount(0);
 
+            mutualFundDataTableGateway.DeleteAll();
+        }
+
+        [Fact]
+        public void ShouldReturnEmptyArrayWhenSymbolIsAndInvestmentVehicleIsFoundButNoPricesAreFound()
+        {
+            var integrationTestsSettings = new IntegrationTestsSettings();
+
+            var mutualFundDataTableGateway = new MutualFundDataTableGateway(integrationTestsSettings);
+            var investmentVehicleDataTableGateway = new InvestmentVehicleDataTableGateway(integrationTestsSettings);
+
+            investmentVehicleDataTableGateway.DeleteAll();
+            mutualFundDataTableGateway.DeleteAll();
+
+            var symbol1 = "SYMB1";
+
+            var mutualFundId = Guid.NewGuid();
+
+            mutualFundDataTableGateway.Insert(new[]{ new MutualFundDto()
+                {
+                    MutualFundId = mutualFundId,
+                    Name = "Some Fund",
+                    Symbol = symbol1
+                }
+            });
+
+            investmentVehicleDataTableGateway.Insert(new []
+            {
+                new InvestmentVehicleDto()
+                {
+                    InvestmentVehicleId = Guid.NewGuid(),
+                    Name = "Some Fund",
+                    ExternalId = mutualFundId
+                }
+            });
+
+            var controller = new PriceController();
+
+            var result = controller.GetAll(new string[] { symbol1 });
+
+            result.Should().HaveCount(0);
+
+            investmentVehicleDataTableGateway.DeleteAll();
             mutualFundDataTableGateway.DeleteAll();
         }
 
