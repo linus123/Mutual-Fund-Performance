e8945252d168af3e355894ea2c4f9dfa9a696a3d
linus123 <linus_pagan@hotmail.com>
2018-05-28 06:59:26 -0500

    Added prices for mutual funds

--- src/TestDataPopulator/Program.cs
+++ src/TestDataPopulator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using MutualFundPerformance.Database.HistoricalPriceData;
 using MutualFundPerformance.Database.MutualFund;
 using MutualFundPerformance.SharedKernel.Infrastructure.HistoricalPriceData;
@@ -11,12 +12,16 @@ namespace MutualFundPerformance.TestDataPopulator
     {
         static void Main(string[] args)
         {
+            var currentDate = DateTime.Now;
+
             var testDataPopulatorSettings = new TestDataPopulatorSettings();
 
             var mutualFundDataTableGateway = new MutualFundDataTableGateway(testDataPopulatorSettings);
             var benchmarkDataTableGateway = new BenchmarkDataTableGateway(testDataPopulatorSettings);
             var investmentVehicleDataTableGateway = new InvestmentVehicleDataTableGateway(testDataPopulatorSettings);
+            var priceDataTableGateway = new PriceDataTableGateway(testDataPopulatorSettings);
 
+            priceDataTableGateway.DeleteAll();
             investmentVehicleDataTableGateway.DeleteAll();
             benchmarkDataTableGateway.DeleteAll();
             mutualFundDataTableGateway.DeleteAll();
@@ -24,10 +29,11 @@ namespace MutualFundPerformance.TestDataPopulator
             var mutualFundDtos = new List<MutualFundDto>();
             var benchmarkDtos = new List<BenchmarkDto>();
             var investmentVehicleDtos = new List<InvestmentVehicleDto>();
+            var priceDtos = new List<PriceDto>();
 
             var random = new Random(100);
 
-            var fundCount = 100;
+            var fundCount = 10;
 
             for (int fundCounter = 0; fundCounter < fundCount; fundCounter++)
             {
@@ -39,12 +45,39 @@ namespace MutualFundPerformance.TestDataPopulator
                     Name = $"Mutual Fund {fundCounter}"
                 });
 
+                var investmentVehicleId = Guid.NewGuid();
+
                 investmentVehicleDtos.Add(new InvestmentVehicleDto()
                 {
-                    ExternalId = Guid.NewGuid(),
-                    InvestmentVehicleId = mutualFundId,
+                    InvestmentVehicleId = investmentVehicleId,
+                    ExternalId = mutualFundId,
                     Name = $"Prices for Mutual Fund {fundCounter}"
                 });
+
+                var dateCounter = currentDate.AddMonths(-121);
+                var currentPrice = random.Next(1, 100000);
+
+                while (dateCounter <= currentDate)
+                {
+                    if (dateCounter.DayOfWeek != DayOfWeek.Saturday && dateCounter.DayOfWeek != DayOfWeek.Sunday)
+                    {
+                        priceDtos.Add(new PriceDto()
+                        {
+                            InvestmentVehicleId = investmentVehicleId,
+                            CloseDate = dateCounter.Date,
+                            Price = currentPrice / 100m
+                        });
+
+                        var change = random.Next(-1000, 1000);
+
+                        currentPrice += change;
+                    }
+                    else
+                    {
+                    }
+
+                    dateCounter = dateCounter.AddDays(1);
+                }
 
                 var benchmarkCount = 1;
 
@@ -88,6 +121,8 @@ namespace MutualFundPerformance.TestDataPopulator
             Console.WriteLine("Inserting investment vehcles.");
             investmentVehicleDataTableGateway.Insert(investmentVehicleDtos.ToArray());
 
+            Console.WriteLine("Inserting prices.");
+            priceDataTableGateway.Insert(priceDtos.ToArray());
         }
     }
 }
