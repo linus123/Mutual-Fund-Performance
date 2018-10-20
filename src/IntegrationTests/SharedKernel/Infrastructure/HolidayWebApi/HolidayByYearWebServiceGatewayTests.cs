bd1d22a669089f41919299bc2c6466bf3c557602
paul.herrera <paul.herrera@dimensional.com>
2018-10-20 04:35:33 -0500

    Added holiday service gateway

---
+++ src/IntegrationTests/SharedKernel/Infrastructure/HolidayWebApi/HolidayByYearWebServiceGatewayTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using MutualFundPerformance.SharedKernel.Infrastructure.HolidayWebApi;
+using Xunit;
+
+namespace MutualFundPerformance.IntegrationTests.SharedKernel.Infrastructure.HolidayWebApi
+{
+    public class HolidayByYearWebServiceGatewayTests
+    {
+        [Fact]
+        public void ShouldReturnValidHolidaysForSingleYear()
+        {
+            var holidayByYearWebServiceGateway = new HolidayByYearWebServiceGateway();
+
+            var holidays = holidayByYearWebServiceGateway.GetHolidays(2018, 2018);
+
+            holidays.Length.Should().BeGreaterOrEqualTo(5);
+            holidays.Length.Should().BeLessOrEqualTo(7);
+
+            foreach (var holiday in holidays)
+            {
+                holiday.DayOfWeek.Should().NotBe(DayOfWeek.Saturday);
+                holiday.DayOfWeek.Should().NotBe(DayOfWeek.Sunday);
+            }
+        }
+    }
+}
---
+++ src/SharedKernel/Infrastructure/HolidayWebApi/HolidayByYearWebServiceGateway.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MutualFundPerformance.SharedKernel.Infrastructure.HolidayWebApi
+{
+    public class HolidayByYearWebServiceGateway
+    {
+        private readonly Random _random;
+
+        public HolidayByYearWebServiceGateway()
+        {
+            _random = new Random();
+        }
+
+        public DateTime[] GetHolidays(
+            int startYear,
+            int endYear)
+        {
+            var holidays = new List<DateTime>();
+
+            for (int yearCounter = startYear; yearCounter <= endYear; yearCounter++)
+            {
+                var holidayCount = _random.Next(5, 7);
+
+                for (int holidayCounter = 0; holidayCounter < holidayCount; holidayCounter++)
+                {
+                    var dayOffset = _random.Next(0, 364);
+
+                    var possibleDate = new DateTime(yearCounter, 1, 1)
+                        .AddDays(dayOffset);
+
+                    while (possibleDate.DayOfWeek == DayOfWeek.Saturday || possibleDate.DayOfWeek == DayOfWeek.Sunday)
+                    {
+                        dayOffset = _random.Next(0, 364);
+
+                        possibleDate = new DateTime(yearCounter, 1, 1)
+                            .AddDays(dayOffset);
+                    }
+
+                    holidays.Add(possibleDate);
+                }
+            }
+
+            return holidays.ToArray();
+        }
+    }
+}
