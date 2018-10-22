6fb947b1f69e943772dc48ce91e10d772f49c5f9
Paul Herrera <pherrera@dimensional.com>
2018-10-22 11:51:37 -0500

    Made progress on mutual fund enpoint

---
+++ src/IntegrationTests/ServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using MutualFundPerformance.Database.MutualFund;
+using MutualFundPerformance.SharedKernel.Infrastructure.MutualFundData;
+using MutualFundPerformance.WebApi.Controllers;
+using Xunit;
+
+
+namespace MutualFundPerformance.IntegrationTests
+{
+    public class ServiceTests
+    {
+
+        [Fact]  
+        public void ShouldGetNoFunds()
+        {
+          
+            var settings = new IntegrationTestsSettings();
+            var mutualFundGateway = new MutualFundDataTableGateway(new IntegrationTestsSettings());
+
+            mutualFundGateway.DeleteAll();
+
+            MutualFundPrice mutualFundPrice = new MutualFundPrice(settings);
+            mutualFundPrice.GetAllFunds().Length.Should().Be(0);
+        }
+
+       
+        [Fact]
+        public void ShouldGetInsertedDataBack()
+        {
+            var settings = new IntegrationTestsSettings();
+            var mutualFundGateway = new MutualFundDataTableGateway(new IntegrationTestsSettings());
+            mutualFundGateway.DeleteAll();
+
+            mutualFundGateway.Insert(new MutualFundDto[]
+            {
+                new MutualFundDto
+                {
+                    MutualFundId = Guid.NewGuid(),
+                    Name = "Mutual Fund 1",
+                    Symbol = "MUTF"
+                }
+            });
+
+            var mutualFundPrice = new MutualFundPrice(settings);
+            mutualFundPrice.GetAllFunds().Length.Should().Be(1);
+            mutualFundPrice.GetAllFunds().Single().Should().Be("Mutual Fund 1");
+        }
+
+        [Fact]
+        public void ShouldReturnFundsInOrder()
+        {
+            var settings = new IntegrationTestsSettings();
+            var mutualFundGateway = new MutualFundDataTableGateway(new IntegrationTestsSettings());
+            mutualFundGateway.DeleteAll();
+
+            mutualFundGateway.Insert(new MutualFundDto[]
+            {
+                new MutualFundDto
+                {
+                    MutualFundId = Guid.NewGuid(),
+                    Name = "Mutual Fund 1",
+                    Symbol = "MUTF"
+                },
+                new MutualFundDto
+                {
+                    MutualFundId = Guid.NewGuid(),
+                    Name = "A Mutual Fund 1",
+                    Symbol = "AMUTF"
+                }
+                ,
+                new MutualFundDto
+                {
+                    MutualFundId = Guid.NewGuid(),
+                    Name = "B Mutual Fund 1",
+                    Symbol = "BMUTF"
+                }
+
+            });
+
+            var mutualFundPrice = new MutualFundPrice(settings);
+            mutualFundPrice.GetAllFunds().Length.Should().Be(3);
+            mutualFundPrice.GetAllFunds()[0].Should().Be("A Mutual Fund 1");
+            mutualFundPrice.GetAllFunds()[1].Should().Be("B Mutual Fund 1");
+            mutualFundPrice.GetAllFunds()[2].Should().Be("Mutual Fund 1");
+        }
+    }
+}
---
+++ src/WebApi/Controllers/MutualFundPrice.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MutualFundPerformance.Database.MutualFund;
+using MutualFundPerformance.SharedKernel;
+
+namespace MutualFundPerformance.WebApi.Controllers
+{
+    public class MutualFundPrice : Controller
+    {
+        private readonly MutualFundDataTableGateway _mutualFundDataTableGateway;
+
+        public MutualFundPrice(
+            IMutualFundPerformanceDatabaseSettings mutualFundPerformanceDatabaseSettings)
+        {
+            _mutualFundDataTableGateway = new MutualFundDataTableGateway(
+                mutualFundPerformanceDatabaseSettings);
+        }
+
+        public string[] GetAllFunds()
+        {
+            var mutualFundDtos = _mutualFundDataTableGateway.GetAll();
+
+            var fundNames = mutualFundDtos
+                .Select(d => d.Name)
+                .OrderBy(n => n)
+                .ToArray();
+
+            return fundNames;
+        }
+    }
+}
